298b83bdb9ae8af82bf6d4aae101e04a4a0bd5ef
jassjy <s4nsy150@gmail.com>
2026-06-24 16:47:42 -0500

    Creacion de tablas facuras y transacciones

--- app/modelos/transacciones.py
+++ app/modelos/transacciones.py
@@ -1,8 +1,9 @@
 from pydantic import BaseModel
+from sqlmodel import SQLModel, Field, Relationship
 
-class TransaccionBase(BaseModel):
-    cantidad: int
-    vr_unitario: float
+class TransaccionBase(SQLModel):
+    cantidad: int = Field(default=0)
+    vr_unitario: float = Field(default=0.0)
     
 class TransaccionCrear(TransaccionBase):
     pass
@@ -10,7 +11,7 @@ class TransaccionCrear(TransaccionBase):
 class TransaccionEditar(TransaccionBase):
     pass
 
-class Transaccion(TransaccionBase):
-    id: int | None = None
-    factura_id: int | None = None
+class Transaccion(TransaccionBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    factura_id: int | None = Field(default=None, foreign_key="factura.id")
     #aqui va la relacion con el modelo factura(solo un campo)
